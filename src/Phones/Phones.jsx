26395a8cf334bdca3231f7df5de12b90c79bac02
mehebul02 <mehebul2123@gmail.com>
2024-03-19 16:27:02 +0600

    loading adding

--- src/Phones/Phones.jsx
+++ src/Phones/Phones.jsx
@@ -1,8 +1,11 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import { ThreeCircles } from 'react-loader-spinner'
 const Phones =()=>{
     const [phones,setPhones] = useState([])
+    const [loading,setLoading]=useState(true)
+
     useEffect(()=>{
         axios.get('https://openapi.programming-hero.com/api/phones?search=iphone')
         .then(data => {
@@ -16,10 +19,22 @@ const Phones =()=>{
                 return obj
             })
             setPhones(phoneWithFakeData)
+            setLoading(false)
         })
     },[])
     return (
 <div>
+<div >
+{loading && <ThreeCircles
+  visible={true}
+  height="100"
+  width="100"
+  color="#4fa94d"
+  ariaLabel="three-circles-loading"
+  wrapperStyle={{}}
+  wrapperClass=""
+  />}
+</div>
     <h1 className="text-3xl">Phones :{phones.length}</h1>
     <BarChart width={750} height={440} data={phones}>
           <Bar dataKey="price" fill="#8884d8" />
